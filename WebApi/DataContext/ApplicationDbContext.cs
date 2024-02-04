d6f74445e9ed4ad31dbd0eb44096a576a6369d66
pitecozz <pedronicas093@gmail.com>
2024-02-04 14:42:40 -0300

    primeiro commit

---
+++ WebApi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }
+
+    }
+}
